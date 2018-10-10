1983d6979c44e215350364a3831cdafcdb507c9b
0mar <omar.morales.alanis.contractor@bbva.com>
2018-10-10 17:15:33 -0500

    Initial commit. Empty Activity template

---
+++ app/src/main/java/com/axiasoft/mycertificationworkout/views/WordsActivity.java
@@ -0,0 +1,63 @@
+package com.axiasoft.mycertificationworkout.views;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.axiasoft.mycertificationworkout.R;
+
+public class WordsActivity extends AppCompatActivity {
+
+    //TODO add support for Define data using Room entities
+    //Access Room database with data access object (DAO)
+    //Observe and respond to changing data using LiveData
+    //Use a Repository to handle data operations
+    //Read and parse raw resources or asset files
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_words);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO launch activity to create a new Item
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_words, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
