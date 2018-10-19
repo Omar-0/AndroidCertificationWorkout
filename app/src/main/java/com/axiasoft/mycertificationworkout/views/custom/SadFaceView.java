7b2f88bf91e5b4d152ac72c0761597a77d8db3ea
0mar <omar.morales.alanis.contractor@bbva.com>
2018-10-19 18:05:29 -0500

    Styleable custom views

--- app/src/main/java/com/axiasoft/mycertificationworkout/views/custom/SadFaceView.java
+++ app/src/main/java/com/axiasoft/mycertificationworkout/views/custom/SadFaceView.java
@@ -1,6 +1,7 @@
 package com.axiasoft.mycertificationworkout.views.custom;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -11,28 +12,91 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
+import com.axiasoft.mycertificationworkout.R;
+
+import static com.axiasoft.mycertificationworkout.views.custom.SadFaceView.Mood.HAPPY;
+import static com.axiasoft.mycertificationworkout.views.custom.SadFaceView.Mood.SAD;
+
 /* Example from https://www.raywenderlich.com/142-android-custom-view-tutorial */
 
 /* custom view draws a happy face*/
 
 public class SadFaceView extends View {
+    /*
+    companion object {
+  private const val DEFAULT_FACE_COLOR = Color.YELLOW
+  private const val DEFAULT_EYES_COLOR = Color.BLACK
+  private const val DEFAULT_MOUTH_COLOR = Color.BLACK
+  private const val DEFAULT_BORDER_COLOR = Color.BLACK
+  private const val DEFAULT_BORDER_WIDTH = 4.0f
+
+  const val HAPPY = 0L
+  const val SAD = 1L
+} */
+
+    private static final int DEFAULT_FACE_COLOR = Color.YELLOW;
+    private static final int DEFAULT_EYES_COLOR = Color.BLACK;
+    private static final int DEFAULT_MOUTH_COLOR = Color.BLACK;
+    private static final int DEFAULT_BORDER_COLOR = Color.BLACK;
+    private static final float DEFAULT_BORDER_WIDTH = 4.0f;
+
+    enum Mood{
+        HAPPY,
+        SAD
+    }
+    private Mood mood;
+    public void setMood(long mood) {
+        if(mood != 0) {
+            invalidate();
+            this.mood = SAD;
+        }
+    }
+
+
 
     // Paint object for coloring and styling
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     // Some colors for the face background, eyes and mouth.
-    private int faceColor = Color.YELLOW;
-    private int eyesColor = Color.BLACK;
-    private int mouthColor = Color.BLACK;
-    private int borderColor = Color.BLACK;
+    private int faceColor = DEFAULT_FACE_COLOR;
+    private int eyesColor = DEFAULT_EYES_COLOR;
+    private int mouthColor = DEFAULT_MOUTH_COLOR;
+    private int borderColor = DEFAULT_BORDER_COLOR;
     // Face border width in pixels
-    private float borderWidth = 4.0f;
+    private float borderWidth = DEFAULT_BORDER_WIDTH;
     // View size in pixels a square view
     private int size = 320;
 
     private Path mouthPath = new Path();
 
+
     public SadFaceView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        paint.setAntiAlias(true);
+        setupAttributes(context, attrs);
+    }
+
+
+    private void setupAttributes(Context context, @Nullable AttributeSet attrs) {
+        // 6
+        // Obtain a typed array of attributes
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SadFaceView,
+                0, 0);
+
+        // 7
+        // Extract custom attributes into member variables
+        int m = typedArray.getInt(R.styleable.SadFaceView_state, 0 );
+        setMood(m);
+        faceColor = typedArray.getColor(R.styleable.SadFaceView_faceColor, DEFAULT_FACE_COLOR);
+        eyesColor = typedArray.getColor(R.styleable.SadFaceView_eyesColor, DEFAULT_EYES_COLOR);
+        mouthColor = typedArray.getColor(R.styleable.SadFaceView_mouthColor, DEFAULT_MOUTH_COLOR);
+        borderColor = typedArray.getColor(R.styleable.SadFaceView_borderColor,
+                DEFAULT_BORDER_COLOR);
+        borderWidth = typedArray.getDimension(R.styleable.SadFaceView_borderWidth,
+                DEFAULT_BORDER_WIDTH);
+
+        // 8
+        // TypedArray objects are shared and must be recycled.
+        typedArray.recycle();
     }
 
     @Override
@@ -57,7 +121,7 @@ public class SadFaceView extends View {
 
         //int desiredWidth = getSuggestedMinimumWidth() + getPaddingLeft() + getPaddingRight();
         //int desiredHeight = getSuggestedMinimumHeight() + getPaddingTop() + getPaddingBottom();
-        
+
         setMeasuredDimension(measureDimension(size, MeasureSpec.getSize(widthMeasureSpec)),
                 measureDimension(size, MeasureSpec.getSize(heightMeasureSpec)));
     }
